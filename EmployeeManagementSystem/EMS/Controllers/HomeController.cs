3660295b7c5b322c872f2237759baa9e4d5bd35a
Gabe-Fernandes <gabrielcatanifernandes@gmail.com>
2023-03-16 14:32:53 -0400

    initial file structure created

---
+++ EmployeeManagementSystem/EMS/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using EMS.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EMS.Controllers
+{
+  public class HomeController : Controller
+  {
+    public HomeController()
+    {
+      
+    }
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+  }
+}
