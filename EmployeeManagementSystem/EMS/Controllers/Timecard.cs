86a0631f32d0792f06cba7851c7ebc96acd5aeba
Gabe-Fernandes <gabrielcatanifernandes@gmail.com>
2023-05-31 23:29:08 -0400

    5/31 html, sass, and some js functionality added for timecard frontend, no identity or responsive mobile design yet

---
+++ EmployeeManagementSystem/EMS/Controllers/Timecard.cs
@@ -0,0 +1,29 @@
+﻿using EMS.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EMS.Controllers;
+
+public class Timecard : Controller
+{
+  public Timecard()
+  {
+    
+  }
+
+  public IActionResult EnterTimecard()
+  {
+    return View();
+  }
+  public IActionResult EditPersonalInfo()
+  {
+    return View();
+  }
+  public IActionResult PersonalInfo()
+  {
+    return View();
+  }
+  public IActionResult ManageUsers()
+  {
+    return View();
+  }
+}
---
+++ EmployeeManagementSystem/EMS/wwwroot/js/ManageUsers.js
@@ -0,0 +1,36 @@
+﻿$(document).ready(function () {
+  const openModal = "O";
+  const closeModal = "C";
+
+  function ToggleModal(main, modal, direction) {
+    if (direction === openModal) {
+      main.addClass("unclickable");
+      modal.removeClass("hide");
+    }
+    else if (direction === closeModal) {
+      main.removeClass("unclickable");
+      modal.addClass("hide");
+    }
+  }
+
+  $("#searchBtn").on("click", () => {
+    $("#searchLbl").text(`Showing search results for: "${$("#searchFirstName").val()} ${$("#searchLastName").val()}"`);
+    if ($("#searchFirstName").val() === "" &&
+      $("#searchLastName").val() === "") {
+      $("#searchLbl").text("Showing all users");
+    }
+  });
+
+  $("#removeUserCloseBtn").on("click", () => {
+    ToggleModal($("#manageUsers"), $("#removeUserModal"), closeModal);
+  });
+  $("#removeUserCancelBtn").on("click", () => {
+    ToggleModal($("#manageUsers"), $("#removeUserModal"), closeModal);
+  });
+  $("#remove1").on("click", () => {
+    ToggleModal($("#manageUsers"), $("#removeUserModal"), openModal);
+  });
+  $("#remove2").on("click", () => {
+    ToggleModal($("#manageUsers"), $("#removeUserModal"), openModal);
+  });
+});
