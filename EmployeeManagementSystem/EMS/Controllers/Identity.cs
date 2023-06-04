6fca3899de445cb80c89425f6ff9fd6dc3f8c4c9
Gabe-Fernandes <gabrielcatanifernandes@gmail.com>
2023-06-03 22:48:23 -0400

    6/3/23 identity frontend setup, no validation or responsive design yet. modal functionality abstracted to Site.js

---
+++ EmployeeManagementSystem/EMS/Controllers/Identity.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EMS.Controllers;
+public class Identity : Controller
+{
+  public IActionResult Login()
+  {
+    return View();
+  }
+  public IActionResult Register()
+  {
+    return View();
+  }
+  public IActionResult ResetPassword()
+  {
+    return View();
+  }
+}
