644bd567ce74386e234e8f22db6fa626746e8b0c
Gabe-Fernandes <gabrielcatanifernandes@gmail.com>
2023-06-29 18:46:04 -0400

    6/29/23 pagination for mobile version of Register View

--- EmployeeManagementSystem/EMS/wwwroot/js/Identity/Register.js
+++ EmployeeManagementSystem/EMS/wwwroot/js/Identity/Register.js
@@ -71,4 +71,19 @@
     TogglePasswordShow($("#registerPassword"), $("#showPassBtn"), $("#hidePassBtn"));
     TogglePasswordShow($("#registerConfirmPassword"), $("#showRepeatPassBtn"), $("#hideRepeatPassBtn"));
   });
+
+  // Mobile Pagination
+
+  $("#mobilePaginationRight1").on("click", () => {
+    $("#mobilePage2").css("left", "0vw");
+  });
+  $("#mobilePaginationLeft2").on("click", () => {
+    $("#mobilePage2").css("left", "100vw");
+  });
+  $("#mobilePaginationRight2").on("click", () => {
+    $("#mobilePage3").css("left", "0vw");
+  });
+  $("#mobilePaginationLeft3").on("click", () => {
+    $("#mobilePage3").css("left", "100vw");
+  });
 });
