b628dd6691081b2642de87607c5d5718ca920ae1
Gabe-Fernandes <gabrielcatanifernandes@gmail.com>
2023-06-30 10:58:09 -0400

    6/29/23 Mobile pagination horizontal overflow (HO) and visible hidden panels fixed, font sizes generally decreased, table HO and manageusers HO fixed

--- EmployeeManagementSystem/EMS/wwwroot/js/Timecard/EditPersonalInfo.js
+++ EmployeeManagementSystem/EMS/wwwroot/js/Timecard/EditPersonalInfo.js
@@ -51,18 +51,47 @@
     $("#editPersonalInfoPhoneNumber").val(phoneNumber);
   }
 
+  // Hide 2nd and 3rd pages when switching to paginated mobile mode
+
+  function hidePaginatedContent() {
+    // if entering mobile mode
+    if ($(window).width() <= 768) {
+      if (!$("#mobilePage2").hasClass("hide") && !$("#mobilePage3").hasClass("hide")) {
+        $("#mobilePage2").addClass("hide");
+        $("#mobilePage3").addClass("hide");
+      }
+    }
+    // if exiting mobile mode
+    else {
+      $("#mobilePage1").removeClass("hide");
+      $("#mobilePage2").removeClass("hide");
+      $("#mobilePage3").removeClass("hide");
+    }
+  }
+  // execute once on each page load
+  hidePaginatedContent();
+  $(window).on("resize", hidePaginatedContent);
+
   // Mobile Pagination
 
   $("#mobilePaginationRight1").on("click", () => {
-    $("#mobilePage2").css("left", "0vw");
+    $("#mobilePage1").addClass("hide");
+    $("#mobilePage2").removeClass("hide");
+    $("#mobilePage3").addClass("hide");
   });
   $("#mobilePaginationLeft2").on("click", () => {
-    $("#mobilePage2").css("left", "100vw");
+    $("#mobilePage1").removeClass("hide");
+    $("#mobilePage2").addClass("hide");
+    $("#mobilePage3").addClass("hide");
   });
   $("#mobilePaginationRight2").on("click", () => {
-    $("#mobilePage3").css("left", "0vw");
+    $("#mobilePage1").addClass("hide");
+    $("#mobilePage2").addClass("hide");
+    $("#mobilePage3").removeClass("hide");
   });
   $("#mobilePaginationLeft3").on("click", () => {
-    $("#mobilePage3").css("left", "100vw");
+    $("#mobilePage1").addClass("hide");
+    $("#mobilePage2").removeClass("hide");
+    $("#mobilePage3").addClass("hide");
   });
 });
